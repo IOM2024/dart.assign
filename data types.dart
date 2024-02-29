4299c63d3b20aa28436f35975c87fc66c6631c5d
IOM2024 <onsaseinnocent@gmail.com>
2024-02-29 20:53:55 +0300

    first commit

---
+++ data types.dart
@@ -0,0 +1,11 @@
+void main(){
+   Map<string, dynamic>mybio={
+      'Name':"ONSASE INNOCENT",
+      "Age" : 27,
+      "City" : "Nairobi",
+      "County" : "Kisii",
+   };
+   print(mybio["Name"]);
+   
+
+}
